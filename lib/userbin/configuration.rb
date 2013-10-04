d5588c14fc517a0a5d85d09ded16e19addd42537
Johan Brissmyr <brissmyr@gmail.com>
2013-10-04 11:55:17 +0200

    Set default credentials based on ENV

--- lib/userbin/configuration.rb
+++ lib/userbin/configuration.rb
@@ -4,5 +4,10 @@ module Userbin
     attr_accessor :api_secret
     attr_accessor :user_model
     attr_accessor :restricted_path
+
+    def initialize
+      self.app_id = ENV["USERBIN_APP_ID"]
+      self.api_secret = ENV["USERBIN_API_SECRET"]
+    end
   end
 end
