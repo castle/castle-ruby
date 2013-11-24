1827d4f5ed962d3950b01b76d91159924971dce3
Johan Brissmyr <brissmyr@gmail.com>
2013-11-24 21:11:50 +0100

    Configuration present in the ENV should always take precedence over that set in code

--- lib/userbin/configuration.rb
+++ lib/userbin/configuration.rb
@@ -1,22 +1,33 @@
 module Userbin
   class Configuration
-    attr_accessor :app_id
-    attr_accessor :api_secret
-    attr_accessor :skip_script_injection
     attr_accessor :create_user
     attr_accessor :find_user
     attr_accessor :protected_path
     attr_accessor :root_path
+    attr_accessor :skip_script_injection
 
     # restricted_path is obsolete
     alias :restricted_path :protected_path
     alias :restricted_path= :protected_path=
 
     def initialize
-      self.app_id = ENV["USERBIN_APP_ID"]
-      self.api_secret = ENV["USERBIN_API_SECRET"]
-
       self.skip_script_injection = false
+    end
+
+    def app_id
+      ENV['USERBIN_APP_ID'] || @_app_id
+    end
+
+    def app_id=(value)
+      @_app_id = value
+    end
+
+    def api_secret
+      ENV['USERBIN_API_SECRET'] || @_api_secret
+    end
+
+    def api_secret=(value)
+      @_api_secret = value
     end
   end
 end
