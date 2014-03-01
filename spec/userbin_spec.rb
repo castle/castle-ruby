1bc898d8c8365bce22dfaed01f626e4c111b5a8c
Sebastian Wallin <sebastian.wallin@gmail.com>
2014-03-01 10:15:20 -0800

    Fix tests

--- spec/userbin_spec.rb
+++ spec/userbin_spec.rb
@@ -9,12 +9,6 @@ describe Userbin do
     end
   end
 
-  let (:args) do
-    {
-      "HTTP_COOKIE" => "_ubt=#{JWT.encode(session, 'abcd', 'HS256')} "
-    }
-  end
-
   let (:session) do
     {
       "id" => 'xyz',
@@ -25,48 +19,46 @@ describe Userbin do
     }
   end
 
-  let (:request) do
-    Rack::Request.new({
-      'CONTENT_TYPE' => 'application/json',
-      'rack.input' => StringIO.new()
-    }.merge(args))
-  end
-
   let (:response) do
     Rack::Response.new("", 200, {})
+  end
+
+  let (:jwt) do
+    JWT.encode(session, 'abcd', 'HS256')
+  end
+
+  let (:jwt_bad) do
+    JWT.encode(session, 'incorrect', 'HS256')
   end
 
   context 'when session is created' do
 
     it 'authenticates with class methods' do
-      Userbin.authenticate!(request)
+      Userbin.authenticate!(jwt)
       Userbin.should be_authenticated
       Userbin.current_user.id.should == "abc"
     end
 
     it 'renews' do
       stub_request(:post, /.*userbin\.com.*/).to_return(:status => 200, :body => "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6Inh5eiIsImV4cGlyZXNfYXQiOjE0Nzg5ODE4ODEwMDAsInVzZXIiOnsiaWQiOiJhYmMifX0.dwDvu_duq6gHrjHtuEgIyEzcoaM1o0W2CC998hty-O0")
-      Userbin.authenticate!(request)
+      Userbin.authenticate!(jwt)
       Userbin.current.id.should == 'xyz'
     end
 
     it 'does not renew' do
       stub_request(:post, /.*userbin\.com.*/).to_return(:status => 404)
-      Userbin.authenticate!(request)
+      Userbin.authenticate!(jwt)
     end
 
     it 'authenticate with correct signature' do
       expect {
-        Userbin.authenticate!(request)
+        Userbin.authenticate!(jwt)
       }.not_to raise_error { Userbin::SecurityError }
     end
 
     it 'does not authenticate incorrect signature' do
-      Userbin.configure do |config|
-        config.api_secret = '1234'
-      end
       expect {
-        Userbin.authenticate!(request)
+        Userbin.authenticate!(jwt_bad)
       }.to raise_error { Userbin::SecurityError }
     end
   end
