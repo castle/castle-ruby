25b22c527a343d1ec804190e80f3522ee2a48a87
Johan Brissmyr <brissmyr@gmail.com>
2014-05-01 14:40:39 +0200

    Restructure utils

--- lib/userbin/utils.rb
+++ lib/userbin/utils.rb
@@ -1,24 +1,26 @@
 module Userbin
-  def self.setup_api(api_secret = nil)
-    api_endpoint = ENV.fetch('USERBIN_API_ENDPOINT') {
-      "https://api.userbin.com/v1"
-    }
+  class << self
+    def setup_api(api_secret = nil)
+      api_endpoint = ENV.fetch('USERBIN_API_ENDPOINT') {
+        "https://api.userbin.com/v1"
+      }
 
-    Her::API.setup url: api_endpoint do |c|
-      c.use Userbin::BasicAuth, api_secret
-      c.use Userbin::ContextHeaders
-      c.use FaradayMiddleware::EncodeJson
-      c.use Userbin::JSONParser
-      c.use Faraday::Adapter::NetHttp
+      Her::API.setup url: api_endpoint do |c|
+        c.use Userbin::BasicAuth, api_secret
+        c.use Userbin::ContextHeaders
+        c.use FaradayMiddleware::EncodeJson
+        c.use Userbin::JSONParser
+        c.use Faraday::Adapter::NetHttp
+      end
     end
-  end
 
-  def self.with_context(env, &block)
-    request = Rack::Request.new(env)
-    RequestStore.store[:userbin_headers] = {
-      ip: request.ip,
-      user_agent: request.user_agent
-    }
-    block.call
+    def with_context(env, &block)
+      request = Rack::Request.new(env)
+      RequestStore.store[:userbin_headers] = {
+        ip: request.ip,
+        user_agent: request.user_agent
+      }
+      block.call
+    end
   end
 end
