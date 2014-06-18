8f9faf621a2b04764b22452579ccbb8882d05d11
Johan Brissmyr <brissmyr@gmail.com>
2014-06-18 21:52:44 +0200

    Replicate all API errors

--- lib/userbin/errors.rb
+++ lib/userbin/errors.rb
@@ -1,6 +1,14 @@
 class Userbin::Error < Exception; end
-class Userbin::NotFoundError < Userbin::Error; end
-class Userbin::Forbidden < Userbin::Error; end
-class Userbin::UserUnauthorizedError < Userbin::Error; end
+
+class Userbin::RequestError < Userbin::Error; end
 class Userbin::SecurityError < Userbin::Error; end
 class Userbin::ConfigurationError < Userbin::Error; end
+
+class Userbin::ApiError < Userbin::Error; end
+
+class Userbin::BadRequest < Userbin::ApiError; end
+class Userbin::UnauthorizedError < Userbin::ApiError; end
+class Userbin::ForbiddenError < Userbin::ApiError; end
+class Userbin::NotFoundError < Userbin::ApiError; end
+class Userbin::UserUnauthorizedError < Userbin::ApiError; end
+class Userbin::InvalidParametersError < Userbin::ApiError; end
