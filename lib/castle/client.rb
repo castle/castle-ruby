2b7a6c516b2dcde00178a48e5bb1ac1291fbca3f
Johan Brissmyr <brissmyr@gmail.com>
2015-03-15 00:02:31 -0700

    Handle empty cookie

--- lib/castle/client.rb
+++ lib/castle/client.rb
@@ -33,7 +33,7 @@ module Castle
       @request_context = {
         ip: request.ip,
         user_agent: request.user_agent,
-        cookie_id: cookies['__cid']
+        cookie_id: cookies['__cid'] || ''
       }
     end
 
