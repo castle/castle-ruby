b02a16fbf567ef346d4c477384567dfb54781c4c
Johan Brissmyr <brissmyr@gmail.com>
2016-12-10 16:26:03 -0800

    Support for new endpoints. Error handling missing

--- lib/castle-rb/client.rb
+++ lib/castle-rb/client.rb
@@ -1,18 +1,31 @@
 module Castle
   class Client
 
-    attr_accessor :request_context, :do_not_track
+    attr_accessor :request_context, :do_not_track, :http, :headers
 
     def initialize(request, response, opts = {})
       # Save a reference in the per-request store so that the request
       # middleware in request.rb can access it
       RequestStore.store[:castle] = self
 
+      cookie_id = extract_cookies(request, response)['__cid'] || ''
+      ip = request.ip
+      headers = header_string(request)
+
+      @http = Net::HTTP.new "api.castle.io", 443
+      @http.use_ssl = true
+      @http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      @headers = {
+        "Content-Type" => "application/json",
+        "X-Castle-Cookie-Id" => cookie_id,
+        "X-Castle-Ip" => ip,
+        "X-Castle-Headers" => headers
+      }
+
       @request_context = {
-        ip: request.ip,
-        user_agent: request.user_agent,
-        cookie_id: extract_cookies(request, response)['__cid'] || '',
-        headers: header_string(request)
+        ip: ip,
+        cookie_id: cookie_id,
+        headers: headers
       }
     end
 
@@ -34,16 +47,20 @@ module Castle
       @do_not_track = false
     end
 
-    def authenticate(user_id)
-      Castle::Authentication.create(user_id: user_id)
+    def authenticate(args)
+      request('authenticate', args)
     end
 
-    def authentication(authentication_id)
-      Castle::Authentication.find(authentication_id)
+    def track(args)
+      request('track', args)
     end
 
-    def authentications
-      Castle::Authentication
+    def request(endpoint, args)
+      req = Net::HTTP::Post.new("/v1/#{endpoint}", @headers)
+      req.basic_auth("", Castle.config.api_secret)
+      req.body = args.to_json
+      response = @http.request(req)
+      JSON.parse(response.body, :symbolize_names => true)
     end
 
     private
