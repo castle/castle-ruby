2fc7dea042e4160ef05f718647004b5d9a95a404
bartes <bartesrlz@gmail.com>
2017-07-14 13:45:10 +0200

    configuration specs

--- lib/castle/configuration.rb
+++ lib/castle/configuration.rb
@@ -3,22 +3,37 @@
 module Castle
   # manages configuration variables
   class Configuration
+    SUPPORTED = %i[source_header request_timeout api_secret api_endpoint].freeze
+    REQUEST_TIMEOUT = 30.0
+    API_ENDPOINT = 'https://api.castle.io/v1'
+
     attr_accessor :request_timeout, :source_header
-    attr_reader :api_endpoint
-    attr_writer :api_secret
+    attr_reader :api_secret, :api_endpoint
 
     def initialize
-      @request_timeout = 30.0
-      self.api_endpoint =
-        ENV['CASTLE_API_ENDPOINT'] || 'https://api.castle.io/v1'
-    end
-
-    def api_secret
-      ENV['CASTLE_API_SECRET'] || @api_secret || ''
+      @request_timeout = self.class::REQUEST_TIMEOUT
+      self.api_endpoint = self.class::API_ENDPOINT
+      self.api_secret = ''
     end
 
     def api_endpoint=(value)
-      @api_endpoint = URI(value)
+      @api_endpoint = URI(
+        ENV.fetch('CASTLE_API_ENDPOINT', value)
+      )
+    end
+
+    def api_secret=(value)
+      @api_secret = ENV.fetch('CASTLE_API_SECRET', value).to_s
+    end
+
+    private
+
+    def respond_to_missing?(method_name, _include_private)
+      /^(\w+)=$/ =~ method_name
+    end
+
+    def method_missing(_m, *_args)
+      raise Castle::ConfigurationError, 'there is no such a config'
     end
   end
 end
