05031b279c87801954059b96298c9e87f9baf8db
Tomasz Pajor <tomek@polishgeeks.com>
2017-09-01 17:58:43 +0200

    add failover and reason

--- lib/castle/failover_auth_response.rb
+++ lib/castle/failover_auth_response.rb
@@ -3,15 +3,18 @@
 module Castle
   # generate failover authentication response
   class FailoverAuthResponse
-    def initialize(user_id, failover_strategy = Castle.config.failover_strategy)
-      @failover_strategy = failover_strategy
+    def initialize(user_id, strategy: Castle.config.failover_strategy, reason:)
+      @strategy = strategy
+      @reason = reason
       @user_id = user_id
     end
 
     def generate
       {
-        'action' => @failover_strategy.to_s,
-        'user_id' => @user_id
+        'action' => @strategy.to_s,
+        'user_id' => @user_id,
+        'failover' => true,
+        'failover_reason' => @reason
       }
     end
   end
