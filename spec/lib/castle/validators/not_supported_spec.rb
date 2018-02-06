2a31a1fc1711f3298d278bcc1b80fbdc379b89ce
bartes <bartesrlz@gmail.com>
2018-02-06 20:56:20 +0100

    validator specs

---
+++ spec/lib/castle/validators/not_supported_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+describe Castle::Validators::NotSupported do
+  describe '#call' do
+    subject(:call) { described_class.call({ first: 1 }, keys) }
+
+    context 'when keys is present' do
+      let(:keys) { %i[first second] }
+
+      it do
+        expect do
+          call
+        end.to raise_error(
+          Castle::InvalidParametersError,
+          'first is/are not supported in identify calls'
+        )
+      end
+    end
+
+    context 'when key is not present' do
+      let(:keys) { %i[second third] }
+
+      it { expect { call }.not_to raise_error }
+    end
+  end
+end
