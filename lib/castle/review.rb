150a6ec9a5082be0a1d8c8a501c3bcd82f7a3a33
bartes <bartesrlz@gmail.com>
2018-02-07 16:20:30 +0100

    no need to initialize Review command class

--- lib/castle/review.rb
+++ lib/castle/review.rb
@@ -3,7 +3,7 @@
 module Castle
   class Review
     def self.retrieve(review_id)
-      command = Castle::Commands::Review.new.build(review_id)
+      command = Castle::Commands::Review.build(review_id)
 
       API.new.request(command)
     end
