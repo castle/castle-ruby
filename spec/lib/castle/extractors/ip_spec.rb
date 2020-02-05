1da06ab70493c7f43e908c5226873e4e003a3a4d
Johanna Larsson <johanna.a.m.larsson@gmail.com>
2020-02-05 15:22:42 +0100

    Fix line length

--- spec/lib/castle/extractors/ip_spec.rb
+++ spec/lib/castle/extractors/ip_spec.rb
@@ -7,7 +7,10 @@ describe Castle::Extractors::IP do
 
   describe 'ip' do
     context 'when regular ip' do
-      let(:env) { Rack::MockRequest.env_for('/', 'HTTP_X_FORWARDED_FOR' => '1.1.1.1, 1.2.2.2, 1.2.3.5') }
+      let(:env) {
+        Rack::MockRequest.env_for('/',
+          'HTTP_X_FORWARDED_FOR' => '1.1.1.1, 1.2.2.2, 1.2.3.5')
+      }
 
       it { expect(extractor.call).to eql('1.2.3.5') }
     end
