bf6a89973634dedf9dfcedae18db588159699904
Johanna Larsson <johanna.a.m.larsson@gmail.com>
2020-02-05 15:41:27 +0100

    formatting?

--- spec/lib/castle/extractors/ip_spec.rb
+++ spec/lib/castle/extractors/ip_spec.rb
@@ -7,10 +7,13 @@ describe Castle::Extractors::IP do
 
   describe 'ip' do
     context 'when regular ip' do
-      let(:env) {
-        Rack::MockRequest.env_for('/',
-          'HTTP_X_FORWARDED_FOR' => '1.1.1.1, 1.2.2.2, 1.2.3.5')
-      }
+      let(:env) do
+        Rack::MockRequest.env_for(
+          '/',
+          'HTTP_X_FORWARDED_FOR' => '1.1.1.1, 1.2.2.2, 1.2.3.5'
+        )
+      end
+
 
       it { expect(extractor.call).to eql('1.2.3.5') }
     end
