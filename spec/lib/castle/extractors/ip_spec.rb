5f3c32a4ccdf9d127cbe1e3e2a04bfe77704b077
Johanna Larsson <johanna.a.m.larsson@gmail.com>
2020-02-05 14:53:30 +0100

    Fix tests

--- spec/lib/castle/extractors/ip_spec.rb
+++ spec/lib/castle/extractors/ip_spec.rb
@@ -7,21 +7,9 @@ describe Castle::Extractors::IP do
 
   describe 'ip' do
     context 'when regular ip' do
-      let(:env) { Rack::MockRequest.env_for('/', 'HTTP_X_FORWARDED_FOR' => '1.2.3.5') }
+      let(:env) { Rack::MockRequest.env_for('/', 'HTTP_X_FORWARDED_FOR' => '1.1.1.1, 1.2.2.2, 1.2.3.5') }
 
       it { expect(extractor.call).to eql('1.2.3.5') }
-    end
-
-    context 'when cf remote_ip' do
-      let(:env) do
-        Rack::MockRequest.env_for(
-          '/',
-          'HTTP_CF_CONNECTING_IP' => '1.2.3.4',
-          'HTTP_X_FORWARDED_FOR' => '1.2.3.5'
-        )
-      end
-
-      it { expect(extractor.call).to eql('1.2.3.4') }
     end
   end
 end
