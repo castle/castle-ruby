f3250623d6afbcd9b439816585a93f5b52b25d3a
bartes <bartesrlz@gmail.com>
2020-04-24 13:56:30 +0200

    fixed problem with symbols in env

--- spec/lib/castle/extractors/headers_spec.rb
+++ spec/lib/castle/extractors/headers_spec.rb
@@ -5,17 +5,18 @@ describe Castle::Extractors::Headers do
 
   let(:client_id) { 'abcd' }
   let(:env) do
-    Rack::MockRequest.env_for(
+    result = Rack::MockRequest.env_for(
       '/',
       'Action-Dispatch.request.content-Type' => 'application/json',
       'HTTP_AUTHORIZATION' => 'Basic 123456',
       'HTTP_COOKIE' => "__cid=#{client_id};other=efgh",
-      'HTTP_OK' => 'OK',
       'HTTP_ACCEPT' => 'application/json',
       'HTTP_X_FORWARDED_FOR' => '1.2.3.4',
       'HTTP_USER_AGENT' => 'Mozilla 1234',
       'TEST' => '1'
     )
+    result[:HTTP_OK] = 'OK'
+    result
   end
   let(:request) { Rack::Request.new(env) }
 
