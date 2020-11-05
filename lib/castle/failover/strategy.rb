8e70fc7b756208690532aa8cbe61b401b8e40f69
bartes <bartesrlz@gmail.com>
2020-11-05 13:09:12 +0100

    added specs

--- lib/castle/failover/strategy.rb
+++ lib/castle/failover/strategy.rb
@@ -2,13 +2,19 @@
 
 module Castle
   module Failover
+    # handles failover strategy consts
     module Strategy
+      # allow
       ALLOW = :allow
+      # deny
       DENY = :deny
+      # challenge
       CHALLENGE = :challenge
+      # throw an error
       THROW = :throw
     end
 
+    # list of possible strategies
     STRATEGIES = %i[allow deny challenge throw].freeze
   end
 end
--- lib/castle/verdict.rb
+++ lib/castle/verdict.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 module Castle
+  # handles verdict consts
   module Verdict
+    # allow
     ALLOW = 'allow'
+    # deny
     DENY = 'deny'
+    # challenge
     CHALLENGE = 'challenge'
   end
 end
