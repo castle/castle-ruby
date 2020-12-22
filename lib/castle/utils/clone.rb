6eacc60d38a9771e9838fa27a33ec85e47259e94
marysieek <maria.korlotian@castle.io>
2020-12-22 16:29:30 +0100

    Add more docs

--- lib/castle/utils/clone.rb
+++ lib/castle/utils/clone.rb
@@ -2,8 +2,10 @@
 
 module Castle
   module Utils
+    # Clones any object
     class Clone
       class << self
+        # Returns a cloned object of any type
         def call(object)
           Marshal.load(Marshal.dump(object))
         end
