31a2ab2b2f53d4fdfd5979a13b1c5d3626a2aa8b
marysieek <maria.korlotian@castle.io>
2021-01-14 12:41:52 +0100

    Further refactor

--- lib/castle/ip/extract.rb
+++ lib/castle/ip/extract.rb
@@ -12,12 +12,12 @@ module Castle
       private_constant :DEFAULT
 
       # @param headers [Hash]
-      def initialize(headers)
+      def initialize(headers, config: Castle.config)
         @headers = headers
-        @ip_headers = Castle.config.ip_headers.empty? ? DEFAULT : Castle.config.ip_headers
-        @proxies = Castle.config.trusted_proxies + Castle::Configuration::TRUSTED_PROXIES
-        @trust_proxy_chain = Castle.config.trust_proxy_chain
-        @trusted_proxy_depth = Castle.config.trusted_proxy_depth
+        @ip_headers = config.ip_headers.empty? ? DEFAULT : config.ip_headers
+        @proxies = config.trusted_proxies + Castle::Configuration::TRUSTED_PROXIES
+        @trust_proxy_chain = config.trust_proxy_chain
+        @trusted_proxy_depth = config.trusted_proxy_depth
       end
 
       # Order of headers:
