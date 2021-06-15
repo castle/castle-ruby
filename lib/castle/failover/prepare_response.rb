b8ef1cff6a864035fdb60838c88c874ff4a97179
James Stoup <james.stoup@castle.io>
2021-06-15 12:05:50 -0400

    coditsu

--- lib/castle/failover/prepare_response.rb
+++ lib/castle/failover/prepare_response.rb
@@ -14,7 +14,7 @@ module Castle
         {
           # v1/risk v1/filter structure
           policy: {
-            action: @strategy.to_s,
+            action: @strategy.to_s
           },
           # v1/authenticate structure
           action: @strategy.to_s,
