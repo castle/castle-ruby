fa55dae10a42c2a5416485608fb9332caca901d6
Sebastian Zuchmański <sebastian.zuchmanski@castle.io>
2024-08-12 09:08:06 +0200

    Use Rails 7.1 in tests

--- spec/integration/rails/support/application.rb
+++ spec/integration/rails/support/application.rb
@@ -3,8 +3,8 @@
 require 'action_controller/railtie'
 
 class TestApp < Rails::Application
-  secrets.secret_token = 'secret_token'
-  secrets.secret_key_base = 'secret_key_base'
+  credentials.secret_token = 'secret_token'
+  credentials.secret_key_base = 'secret_key_base'
 
   config.logger = Logger.new($stdout)
   Rails.logger = config.logger
