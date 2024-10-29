edd1077a8575b8b1dabc9bc35423a593ff96211d
Andy Dymond <andy.dymond@housecallpro.com>
2024-10-29 11:02:49 -0600

    Add list commands

---
+++ lib/castle/commands/list/get.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Castle
+  module Commands
+    module List
+      class Get
+        class << self
+          # @param options [Hash]
+          # @return [Castle::Command]
+          def build(options = {})
+            Castle::Validators::Present.call(options, %i[list_id])
+
+            Castle::Command.new("lists/#{options[:list_id]}", nil, :get)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/castle/commands/list/create_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+describe Castle::Commands::List::Create do
+  describe '.build' do
+    subject(:command) { described_class.build(options) }
+
+    let(:options) { { name: 'list_name', color: '$green', primary_field: 'user.email' } }
+
+    context 'with valid options' do
+      it { expect(command.method).to be(:post) }
+      it { expect(command.path).to eql('lists') }
+      it { expect(command.data).to eql(options) }
+    end
+
+    context 'without name' do
+      let(:options) { { color: '$green', primary_field: 'user.email' } }
+
+      it { expect { command }.to raise_error(Castle::InvalidParametersError) }
+    end
+
+    context 'without color' do
+      let(:options) { { name: 'list_name', primary_field: 'user.email' } }
+
+      it { expect { command }.to raise_error(Castle::InvalidParametersError) }
+    end
+
+    context 'without primary_field' do
+      let(:options) { { name: 'list_name', color: '$green' } }
+
+      it { expect { command }.to raise_error(Castle::InvalidParametersError) }
+    end
+  end
+end
