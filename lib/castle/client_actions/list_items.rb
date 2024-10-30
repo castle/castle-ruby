ff517bad6d09c9b326cf209260875b5abde7f0ce
Andy Dymond <andy.dymond@housecallpro.com>
2024-10-30 15:26:40 -0600

    update client action calls to have option signature of apis called

--- lib/castle/client_actions/list_items.rb
+++ lib/castle/client_actions/list_items.rb
@@ -6,19 +6,19 @@ module Castle
         Castle::API::ListItems::Archive.call(options)
       end
 
-      def count_list_items(options)
+      def count_list_items(options = {})
         Castle::API::ListItems::Count.call(options)
       end
 
-      def create_list_item(options)
+      def create_list_item(options = {})
         Castle::API::ListItems::Create.call(options)
       end
 
-      def get_list_item(options)
+      def get_list_item(options = {})
         Castle::API::ListItems::Get.call(options)
       end
 
-      def query_list_items(options)
+      def query_list_items(options = {})
         Castle::API::ListItems::Query.call(options)
       end
 
@@ -26,7 +26,7 @@ module Castle
         Castle::API::ListItems::Unarchive.call(options)
       end
 
-      def update_list_item(options)
+      def update_list_item(options = {})
         Castle::API::ListItems::Update.call(options)
       end
     end
